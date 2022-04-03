c9e4ee8e21fb18ba4399e4d8583610aa3ed17188
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-04-03 07:43:50 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,180 @@
+/*
+    Sourav Kumar Dubey
+*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli               long long int
+#define forn(i,x,n)       for (i = x; i <= n; i++)
+#define rev(n,i)          for(i=N-1;i>=0;i--)
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pb                push_back
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+ 
+const long long INF=1e18;
+const long long int M=1e9+1;
+const int32_t MM=998244353;
+ 
+/*int power(int x, int n) 
+{
+    int res = 1;
+    
+    while (n > 0) {
+        
+        if (n & 1) res = (res * x);
+        x = (x * x);
+        n >>= 1;
+ 
+    }
+    
+    return res;
+}*/
+
+/*vector < int > seive;
+
+void SieveOfEratosthenes(int n)
+{
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+  
+    for (int p=2; p*p<=n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+  
+    for (int p=2; p<=n; p++)
+      if (prime[p])
+          seive.push_back(p);
+}*/
+ 
+/*
+int lengthOfLIS(vector<lli>& nums) {
+    int n = nums.size();
+    set<int> se;
+    int flag = 0;
+    for(int i = 0; i<n; ++i){
+        if(se.lower_bound(nums[i]) != se.end()){
+            se.erase(se.lower_bound(nums[i]));
+        }
+        se.insert(nums[i]);
+    }
+    return se.size() ;
+}*/
+ 
+//------------------------------------------------------------------------------
+ 
+void insert ( vector < lli > &vec , lli n ) {
+    
+    for ( int i = 0 ; i < n ; i++ ) {
+        
+        cin >> vec[i];
+        
+    }
+    
+}
+
+void print ( vector < lli > &vec , lli n ) {
+    
+    for ( int i = 0 ; i < n ; i++ ) {
+        
+        cout << vec[i] << " ";
+        
+    }
+    
+    cout << "\n";
+    
+}
+
+bool isPrime(lli n) {
+    
+    for ( int i = 2 ; i <= sqrt(n) ; i++ ) {
+        
+        if ( n % i ) continue;
+        else return false;
+        
+    }
+    
+    return true;
+    
+}
+
+// const lli mod = 998244353 ;
+
+void test_case() {
+    
+    lli n;
+    cin >> n;
+    
+    vector < lli > vec(n);
+    insert(vec , n);
+    
+    if ( n == 2 ) {
+        
+        cout << 2 << "\n";
+        
+    }
+    else if ( n == 1 ) {
+        
+        cout << 3 << "\n";
+        
+    }
+    else {
+        
+        map < lli , int > mp;
+        lli cnt = 0;
+        
+        for ( auto i : vec ) mp[i] += 1;
+        
+        for ( auto &i : mp ) {
+            
+            if ( i.second % 2 != 0 ) {
+
+                cnt++;
+                
+            }
+            
+        }
+        
+        while ( ( n + cnt ) % 4 != 0 ) {
+            
+            cnt++;
+            
+        }
+        
+        cout << cnt << "\n";
+
+    }
+    return;
+    
+}
+
+signed main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    
+    
+    int t = 1;  
+    cin >> t;
+    
+    while ( t-- ) {
+    
+      test_case();
+      
+    }
+    return 0;
+}
